70e75b0c1580697a3fe8a1b0d345cc3e9198584b
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-07-10 00:49:31 +0530

    add upto routing

---
+++ src/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="navbar bg-gray-500 shadow-lg"> {/* Changed to bg-base-300 for better theme compatibility */}
+      <div className="flex-1">
+        <a href="/" className="btn btn-ghost text-xl">DevTinder</a>
+      </div>
+
+      <div className="flex-none gap-2"> {/* Changed to flex-none for better alignment */}
+        <div className="form-control">
+          <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+        </div>
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="User profile picture"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            // Using z-[1] to ensure dropdown appears above other content.
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
