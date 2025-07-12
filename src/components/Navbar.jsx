6f8c204c9461678e1cd93b7498996a995118875c
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-07-12 23:56:45 +0530

    ad link on navbar for profile and Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const user = useSelector((state) => state.user);
@@ -6,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-gray-500 shadow-lg"> {/* Changed to bg-base-300 for better theme compatibility */}
       <div className="flex-1">
-        <a href="/" className="btn btn-ghost text-xl">DevTinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
       </div>
     {user &&(
       <div className="flex-none gap-2"> {/* Changed to flex-none for better alignment */}
@@ -28,10 +29,10 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
+              <Link to="/profile" className="justify-between">
                 Profile
                 <span className="badge">New</span>
-              </a>
+              </Link>
             </li>
             <li><a>Settings</a></li>
             <li><a>Logout</a></li>
