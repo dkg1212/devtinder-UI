bed6a50a30ad9799c6e0ac925a6a31d75474f0a3
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-07-15 21:11:56 +0530

    add feat:accept/reject request

---
+++ src/components/Requests.jsx
@@ -0,0 +1,106 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addRequests, removeRequest } from "../utils/requestSlice";
+import { useEffect } from "react";
+
+const Requests = () => {
+  const requests = useSelector((store) => store.requests);
+  const dispatch = useDispatch();
+
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeRequest(_id));
+    } catch (err) {
+      console.error("Error reviewing request:", err);
+      // Handle Error Case
+      if (err.response && err.response.status === 400) {
+        console.error("Invalid request data");
+      } else {
+        console.error("An error occurred while processing the request.");
+      }
+    }
+  };
+
+  const fetchRequests = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/requests/received", {
+        withCredentials: true,
+      });
+
+      dispatch(addRequests(res.data.data));
+    } catch (err) {
+      console.error("Error fetching requests:", err);
+      // Handle Error Case
+      if (err.response && err.response.status === 400) {
+        console.error("Invalid request data");
+      } else {
+        console.error("An error occurred while fetching requests.");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  if (!requests) return;
+
+  if (requests.length === 0)
+    return <h1 className="flex justify-center my-10"> No Requests Found</h1>;
+
+  return (
+    <div className="text-center my-10">
+      <h1 className="text-bold text-white text-3xl">Connection Requests</h1>
+
+      {requests.map((request) => {
+        const { _id, firstName, lastName, photoUrl, age, gender, about } =
+          request.fromUserID;
+
+        return (
+          <div
+            key={_id}
+            className=" flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/3 mx-auto"
+          >
+            <div>
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full"
+                src={photoUrl}
+              />
+            </div>
+            <div className="text-left mx-4 ">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+              {age && gender && <p>{age + ", " + gender}</p>}
+              <p>{about}</p>
+            </div>
+            <div>
+              <button
+                className="btn btn-primary mx-2"
+                onClick={() => reviewRequest("rejected", request._id)}
+              >
+                Reject
+              </button>
+              <button
+                className="btn btn-secondary mx-2"
+                onClick={() => reviewRequest("accepted", request._id)}
+              >
+                Accept
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default Requests;
