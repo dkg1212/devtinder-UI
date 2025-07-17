ffba901fb4df0d9cbb64a0975772107eeb704c8a
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-07-17 19:34:40 +0530

    feat:interested/ignore in feed usecards

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -32,6 +32,9 @@ const Feed = () => {
       getFeed();
   }, []);
 
+  if (!feed) return;
+  if (feed.length === 0) return <h1 className="flex justify-center my-10">No Users Found</h1>;
+  
   return (
     feed && (
       <div className="flex justify-center my-10">
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,5 +1,33 @@
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+
 const UserCard = ({ user }) => {
-  const { firstName, lastName, age, gender, about } = user;
+  const { _id ,firstName, lastName, age, gender, about } = user;
+  const dispatch=   useDispatch();
+
+  const handleSendRequest = async(status , userId) => {
+    try {  
+        const res = await axios.post(
+            BASE_URL + "/request/send/" + status + "/" + userId,
+            {},
+            { withCredentials: true }
+        );
+        if (res.status === 200) {
+            dispatch(removeUserFromFeed(userId));
+        }
+        }catch (err) {
+        console.error("Error sending request:", err);
+        // Handle Error Case
+        if (err.response && err.response.status === 400) {
+            console.error("Invalid request data");
+        } else {
+            console.error("An error occurred while sending the request.");
+        }
+    }
+  };   
+   
   return (
     <div className="card bg-base-300 w-96 shadow-xl">
       <figure>
@@ -10,8 +38,15 @@ const UserCard = ({ user }) => {
         {age && gender && <p>{age + ", " + gender}</p>}
         <p>{about}</p>
         <div className="card-actions justify-center my-4">
-          <button className="btn btn-primary">Ignore</button>
-          <button className="btn btn-secondary">Interested</button>
+          <button 
+          className="btn btn-primary"
+            onClick={() => handleSendRequest("ignore", _id)}>
+            Ignore
+            </button>
+          <button className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", _id)}>
+            Interested
+          </button>
         </div>
       </div>
     </div>
