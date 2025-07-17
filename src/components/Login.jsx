b20b7b001b3bf6a9fc23a020835dfd29ae1ef91f
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-07-17 23:49:22 +0530

    add feat:SignUp and bug fixes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,13 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("Msdhoni@gmail.com");
-  const [password, setPassword] = useState("Hello@123");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const[firstName, setFirstName] = useState("");
+  const[lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [error, setError] = useState("");
@@ -33,13 +38,93 @@ const Login = () => {
       }
     }
   };
+  
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        {
+          firstName,
+          lastName,
+          emailId,
+          password,
+          age,
+          gender,
+        },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (err) {
+      console.error("Signup failed:", err);
+      if (err.response && err.response.status === 400) {
+        setError("Email already exists or invalid data");
+      } else {
+        setError("An error occurred while signing up. Please try again later.");
+      }
+    }
+  };
 
   return (
     <div className="flex justify-center my-10">
       <div className="card bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login</h2>
+          <h2 className="card-title justify-center">
+            {isLoginForm ? "Login" : "Sign Up"}
+          </h2>
           <div>
+            {!isLoginForm && (
+              <>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">First Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={firstName}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">Last Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">Age</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={age}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setAge(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">Gender</span>
+                  </div>
+                  <select
+                    value={gender}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <option value="">Select Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </label>
+              </>
+            )}
             <label className="form-control w-full max-w-xs my-2">
               <div className="label">
                 <span className="label-text">Email ID:</span>
@@ -65,10 +150,20 @@ const Login = () => {
           </div>
           <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center m-2">
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
+            <button className="btn btn-primary" onClick={isLoginForm ? handleLogin : handleSignUp}>
+              {isLoginForm ? "Login" : "Sign Up"}
             </button>
           </div>
+          <p>
+            {isLoginForm ? "Don't have an account? " : "Already have an account? "}
+            <span
+              className="text-blue-500 cursor-pointer"
+              onClick={() => setIsLoginForm(!isLoginForm)}
+            >
+              {isLoginForm ? "Sign Up" : "Login"}
+            </span>
+
+          </p>
         </div>
       </div>
     </div>
